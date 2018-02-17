ef4f80eecaa92a33aae76b3645430f155bca7fe7
Bruno Sinou <bruno.sinou@gmail.com>
2018-02-17 10:53:04 +0100

    Implement first assignment

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # react-training
+
 A personal repository to practice with react
 
+## Prepare environment
+
+### Install necessary third parties
+
+```bash
+# Enable code checking
+sudo npm install -g eslint
+```
+
+In Visual Studio Code, install:
+
+- ESLint
+- React Native Tools
--- assignment-one/src/App.js
+++ assignment-one/src/App.js
@@ -2,11 +2,56 @@ import React, { Component } from 'react';
 import './App.css';
 
 
+const UserInput = (props) => {
+  const inputStyle = {
+    border: '2px solid blue',
+    padding: '2px 5px'  
+  };
+
+  return (
+    <input
+      type='text'
+      style= {inputStyle}
+      onChange={props.change}
+      value={props.name}
+    />
+  );
+};
+
+const UserOutput = (props) => {
+  return (
+    <div>
+      <p>Username: {props.name}</p>
+      <p>With a description</p>
+    </div>
+  );
+};
+
+
 class App extends Component {
+
+  state = {
+    username: 'John'
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState(
+      {
+        username: event.target.value
+      }
+    );
+  }
+
   render() {
+    const currName = this.state.username
     return (
       <div className="App">
-      Here I am
+        <UserInput name={currName} change={this.nameChangedHandler} />
+        <div className="outputs">
+          <UserOutput name={currName} />
+          <UserOutput name={currName} />
+          <UserOutput name={currName} />
+        </div>
       </div>
     );
   }
